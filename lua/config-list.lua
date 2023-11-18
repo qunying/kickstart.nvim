05675234f421ae6c3e8ac5d3f7a809fb9bd05615
Zhu Qun-Ying <zhu.qunying@gmail.com>
2023-11-17 17:06:56 -0800

    turn on autoread

--- lua/config-list.lua
+++ lua/config-list.lua
@@ -5,4 +5,10 @@ require "config.treesitter"
 require "config.lsp"
 require "config.cmp"
 require "config.nvim-tree"
+
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
 -- vim: ts=2 sts=2 sw=2 et
